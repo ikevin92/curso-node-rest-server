8ea29e6fc4d4f752fde2f6d0cd4a753ead83a9b5
Kevin Echeverri <ikevin1992@gmail.com>
2021-08-29 20:33:36 -0500

    add config bd

--- src/models/server.js
+++ src/models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
   constructor() {
@@ -7,10 +8,18 @@ class Server {
     this.port = process.env.PORT;
     this.usuariosPath = '/api/usuarios';
 
+    //conectar a base de datos
+    this.conectarDB();
+
     // middlewares
     this.middlewares();
+
     // rutas
     this.routes();
+  }
+
+  async conectarDB() {
+    await dbConnection();
   }
 
   middlewares() {
